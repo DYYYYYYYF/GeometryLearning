6fcdbb5c8e449929a8cd94a14b5cc6d6b7cab2e9
uncledon <dongyf>
2026-01-05 14:38:27 +0800

    优先寻找各系统默认字体

--- ui/GUI.py
+++ ui/GUI.py
@@ -19,8 +19,10 @@ class Text(UIElement):
         self.color = color
 
         try:
-            self.font = pygame.font.SysFont('Arial', font_size)
+            # Arial (Win), Helvetica (Mac), DejaVu Sans (Linux), Sans (通用)
+            self.font = pygame.font.SysFont(['arial', 'helvetica', 'dejavusans', 'sans'], font_size)
         except FileNotFoundError:
+            print('can not find font, use system default font.')
             self.font = pygame.font.SysFont(None, font_size)
 
     def set_text(self, t):
